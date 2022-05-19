3edff93c726723daee46742cdc8a70e974cf944f
Niklas Nickel <niklas.nickel@rwth-aachen.de>
2022-05-19 23:13:13 +0200

    Reindent code

--- Package.swift
+++ Package.swift
@@ -4,25 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "SI",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "SI",
-            targets: ["SI"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SI",
-            dependencies: []),
-        .testTarget(
-            name: "SITests",
-            dependencies: ["SI"]),
-    ]
+	name: "SI",
+	products: [
+		// Products define the executables and libraries a package produces, and make them visible to other packages.
+		.library(
+			name: "SI",
+			targets: ["SI"]),
+	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		// .package(url: /* package url */, from: "1.0.0"),
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages this package depends on.
+		.target(
+			name: "SI",
+			dependencies: []),
+		.testTarget(
+			name: "SITests",
+			dependencies: ["SI"]),
+	]
 )
--- Tests/SITests/SITests.swift
+++ Tests/SITests/SITests.swift
@@ -4,28 +4,28 @@ import XCTest
 final class SITests: XCTestCase {
 	// SI Handeling
 	func testSIhandeling() throws {
-		 let meters = 1.5[.m]
-		 let millimeters = 400[.mm]
-		 let seconds = 10[.s]
-		 let squaremeters = 25[.m2]
-		 
-		 XCTAssertEqual(meters / seconds, 0.15[.m_s])
-		 XCTAssertEqual(meters / seconds + meters / seconds, 0.3[.m_s])
-		 XCTAssertEqual(meters + millimeters, 1.9[.m])
-		 XCTAssertEqual(squaremeters + meters * meters, 27.25[.m2])
-		 XCTAssertEqual(meters + millimeters, 1900[.mm])
-		 XCTAssertEqual(meters ^ 2, 2.25[.m2])
-		 XCTAssertEqual(meters * meters, 2.25[.m2])
-		 XCTAssertEqual(meters * millimeters, 0.6[.m2])
-		 XCTAssertEqual(SI.sqrt(meters * meters), meters)
-		 XCTAssertEqual(SI.sqrt(squaremeters), 5[.m])
-		 XCTAssertEqual(SI.sqrt(-meters ^ 2), meters)
-		 XCTAssertNil(SI.sqrt(-(meters ^ 2)))
-//        try XCTAssertThrowsError(SI.sqrt(meters))
-//        try XCTAssertThrowsError(meters.convert(to: Unit("")))
-		 XCTAssertEqual(5.0 * millimeters, 2[.m])
-		 XCTAssertEqual(meters * 2.0, 3[.m])
-		 XCTAssertEqual(1.0 / seconds, 0.1[.Hz])
-		 XCTAssertEqual(seconds / 2.0, 5[.s])
+		let meters = 1.5[.m]
+		let millimeters = 400[.mm]
+		let seconds = 10[.s]
+		let squaremeters = 25[.m2]
+		
+		XCTAssertEqual(meters / seconds, 0.15[.m_s])
+		XCTAssertEqual(meters / seconds + meters / seconds, 0.3[.m_s])
+		XCTAssertEqual(meters + millimeters, 1.9[.m])
+		XCTAssertEqual(squaremeters + meters * meters, 27.25[.m2])
+		XCTAssertEqual(meters + millimeters, 1900[.mm])
+		XCTAssertEqual(meters ^ 2, 2.25[.m2])
+		XCTAssertEqual(meters * meters, 2.25[.m2])
+		XCTAssertEqual(meters * millimeters, 0.6[.m2])
+		XCTAssertEqual(SI.sqrt(meters * meters), meters)
+		XCTAssertEqual(SI.sqrt(squaremeters), 5[.m])
+		XCTAssertEqual(SI.sqrt(-meters ^ 2), meters)
+		XCTAssertNil(SI.sqrt(-(meters ^ 2)))
+		//        try XCTAssertThrowsError(SI.sqrt(meters))
+		//        try XCTAssertThrowsError(meters.convert(to: Unit("")))
+		XCTAssertEqual(5.0 * millimeters, 2[.m])
+		XCTAssertEqual(meters * 2.0, 3[.m])
+		XCTAssertEqual(1.0 / seconds, 0.1[.Hz])
+		XCTAssertEqual(seconds / 2.0, 5[.s])
 	}
 }
